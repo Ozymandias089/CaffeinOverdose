29bbab6af8174044caaa609a10fd176867b4734a
Younghoon Choi <ozymandias089@gmail.com>
2025-09-10 14:44:40 +0900

    Initial Commit

---
+++ CaffeinOverdose/CaffeinOverdoseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaffeinOverdoseApp.swift
+//  CaffeinOverdose
+//
+//  Created by 최영훈 on 9/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CaffeinOverdoseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
